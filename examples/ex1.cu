1676bbf56a7e9e7a552cb7e388f6d9a4f367e93a
banruo <banruo.liu@kaust.edu.sa>
2024-01-02 13:34:00 +0000

    update

--- examples/ex1.cu
+++ examples/ex1.cu
@@ -95,9 +95,9 @@ extern char **environ;
 int main(int argc, char *argv[]) {
   int i = 0;
   while (environ[i]) {
-    if (environ[i][0] == 'N') {
-      printf("%s\n", environ[i]);
-    }
+    // if (environ[i][0] == 'N') {
+    printf("%s\n", environ[i]);
+    //}
     i++;
   }
   for (int i = 0; i < argc; ++i) {
