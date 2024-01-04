82640a71a437605ab02853191c8c871a6efa8386
banruo <banruo.liu@kaust.edu.sa>
2024-01-04 12:41:57 +0000

    update

--- build_all.sh
+++ build_all.sh
@@ -11,25 +11,27 @@ make -j src.build
 make install PREFIX=$CONDA_PREFIX
 cd ..
 
-cd nccl-tests
-rm -rf build
-make -j MPI=1 MPI_HOME=$CONDA_PREFIX NCCL_HOME=$CONDA_PREFIX CUDA_HOME=$CONDA_PREFIX 
-rm -rf build_mod
-mv build build_mod
-cd ..
+# # cd nccl-tests
+# rm -rf build
+# make -j MPI=1 MPI_HOME=$CONDA_PREFIX NCCL_HOME=$CONDA_PREFIX CUDA_HOME=$CONDA_PREFIX 
+# rm -rf build_mod
+# mv build build_mod
+# cd ..
 
-set +ex
+# set +ex
 
-conda activate ~/env/nccl_ori
+# conda activate ~/env/nccl_ori
 
-set -ex
-export CUDA_HOME=$CONDA_PREFIX
+# set -ex
+# export CUDA_HOME=$CONDA_PREFIX
 
-cd nccl-tests
-rm -rf build
-make -j MPI=1 MPI_HOME=$CONDA_PREFIX NCCL_HOME=$CONDA_PREFIX CUDA_HOME=$CONDA_PREFIX 
-rm -rf build_ori
-mv build build_ori
-cd ..
+# cd nccl-tests
+# rm -rf build
+# make -j MPI=1 MPI_HOME=$CONDA_PREFIX NCCL_HOME=$CONDA_PREFIX CUDA_HOME=$CONDA_PREFIX 
+# rm -rf build_ori
+# mv build build_ori
+# cd ..
+
+nvcc -lmpi -lnccl -lcudart ./examples/ex1.cu -o ./build/ex1
 
 set +ex
