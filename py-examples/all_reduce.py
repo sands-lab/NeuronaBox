fefde0964f7bba73ddbb8dccef5cb97ab6d547e0
banruo <banruo.liu@kaust.edu.sa>
2024-02-05 13:47:23 +0000

    nice!

--- py-examples/all_reduce.py
+++ py-examples/all_reduce.py
@@ -2,37 +2,48 @@ import os
 import torch
 import torch.distributed as dist
 
-LOCAL_RANK = int(os.environ['LOCAL_RANK'])
-WORLD_SIZE = int(os.environ['WORLD_SIZE'])
-WORLD_RANK = int(os.environ['RANK'])
+myrank = 0
+nranks = 0
 
 def init():
-  myrank = int(os.environ['RANK'])
-  nranks = int(os.environ['WORLD_SIZE'])
+  global myrank, nranks
+  os.environ["MOD_MY_MPI_RANK"] = str(myrank)
+  os.environ["MOD_N_MPI_RANKS"] = str(nranks)
   if myrank == 0:
     os.environ["MOD_KERNEL_BYPASS"] = "1"
-    os.environ["MOD_MY_NODE"] = str(myrank)
-    os.environ["MOD_NODES"] = str(nranks)
-  os.environ['MOD_N']
+  
+  os.environ["LOCAL_RANK"] = "0"
+  os.environ["RANK"] = str(myrank)
+  os.environ["WORLD_SIZE"] = str(nranks)
+  
+  print("I am rank {} of {}, kbypass={}".format(myrank, nranks, os.environ.get("MOD_KERNEL_BYPASS")))
 
 def all_reduce(tensor):
   """Performs an all-reduce operation on the given tensor."""
   dist.all_reduce(tensor)
 
 def main():
+  global myrank, nranks
+  argc = len(os.sys.argv)
+  assert argc == 4, "Usage: {} <rank> <nranks> <loop>".format(os.sys.argv[0])
+  myrank = int(os.sys.argv[1])
+  nranks = int(os.sys.argv[2])
+  loop = int(os.sys.argv[3])
+  init()
   # Initialize distributed training
-  print("Rank {} Initializing".format(WORLD_RANK))
+  print("Rank {} Initializing".format(myrank))
   dist.init_process_group(backend='nccl')
-  print("Rank {} Initialized".format(WORLD_RANK))
-  device = torch.device("cuda:{}".format(LOCAL_RANK))
-  # Generate a random tensor
-  tensor = torch.rand(1000, dtype=torch.float32, device=device)
-  print("before allreduce", tensor[:10])
-  # Perform all-reduce
-  all_reduce(tensor)
-
-  # Print the result
-  print("after allreduce", tensor[:10])
+  print("Rank {} Initialized".format(myrank))
+  device = torch.device("cuda:{}".format(0))
+  
+  for i in range(loop):
+    # Generate a random tensor
+    tensor = torch.rand(1000, dtype=torch.float32, device=device)
+    print("before allreduce", tensor[:10])
+    # Perform all-reduce
+    all_reduce(tensor)
+    # Print the result
+    print("after allreduce", tensor[:10])
 
 if __name__ == '__main__':
   main()
